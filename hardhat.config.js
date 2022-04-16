07f44072351d26b54e8c70a7bd5834389c5ae733
Zainurin Zainal <zainurin.syafiq@globaltium.com>
2022-04-16 16:33:14 +0800

    feat: rinkeby setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,11 +5,11 @@ require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-ganache");
 
-const { API_URL, PRIVATE_KEY } = process.env;
+const { RINKEBY_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.1",
-  defaultNetwork: "ganache",
+  defaultNetwork: "rinkeby",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545",
@@ -23,8 +23,12 @@ module.exports = {
       chainId: 1337,
     },
     // ropsten: {
-    //   url: API_URL,
+    //   url: ROPSTEN_URL,
     //   accounts: [`0x${PRIVATE_KEY}`],
     // },
+    rinkeby: {
+      url: RINKEBY_URL,
+      accounts: [PRIVATE_KEY],
+    },
   },
 };
